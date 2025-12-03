c904bb586ff33db4c43c963fac86d5c75679f1f7
Phi Viet Huynh <viet.hp.66cntt@ntu.edu.vn>
2025-12-04 02:35:50 +0700

    RandomInput.py app.py

--- RandomInput.py
+++ RandomInput.py
@@ -1,15 +1,40 @@
 import random as rd
 import string
+import Xuly
 def RandQuanHe():
     g=[]
     a=list(string.ascii_lowercase)
-    for i in range(rd.randint(1,8)):
+    for i in range(rd.randint(4,8)):
         g.append(rd.choice(a))
-    return "".join(set(g))
+    return "".join(sorted((set(g)))).upper() #biến list g thành str g, sau đó viết hoa lên rồi sort lại
+
+def ListToText(a:list):
+    return "".join(a)
+
+
 def RandPTH(quanhe):
-    pass
+    quanhe=list(quanhe)
+    luachon=[]
+    temp1=[]
+    temp2=[]
+    for i in range(len(quanhe)):
+        luachon.append(quanhe[i])
+    for i in range(rd.randint(1,3)):
+        temp1.append(rd.choice(luachon))
+    for chu in list(set(temp1)):
+        quanhe.remove(chu)
+    try:
+        for i in range(rd.randint(1,3)):
+            temp2.append(rd.choice(quanhe))
+    except IndexError:
+        return 0
+    
+    temp1=ListToText(list(set(temp1)))
+    temp2=ListToText(list(set(temp2)))
+    value=Xuly.phuthuocham(temp1,temp2)
+    return value
 
 def main():
-    pass
+    RandPTH(RandQuanHe())
 if __name__ == "__main__":
     main()
--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 import streamlit as st
 import Xuly
-# def chuanhoa(PTH:str):
-#     group=[]
-#     handle1=PTH.strip().split(",")      #tách các pth ra
-#     for pth in handle1:     
-#         group.append(Xuly.phuthuocham(pth.strip().split(" ")[0],pth.strip().split(" ")[1]))  #tách vế trái vế phải
-#     return group
+import RandomInput as ran
+def chuanhoa(PTH:str):
+    group=[]
+    handle1=PTH.strip().split(",")      #tách các pth ra
+    for pth in handle1:     
+        group.append(Xuly.phuthuocham(pth.strip().split(" ")[0],pth.strip().split(" ")[1]))  #tách vế trái vế phải
+    return group
 
 st.title("Tìm Khóa Cho 1 Quan hệ")
 
-page=st.sidebar.selectbox("Chọn cách nhập PTH:",["Nhập Thủ Công","Nhập Bằng File"])
+page=st.sidebar.selectbox("Chọn cách nhập PTH:",["Nhập Thủ Công","Nhập Bằng File","Tạo Ngẫu Nhiên"])
 if page=="Nhập Thủ Công":
     name = st.text_input("Quan Hệ")
     st.markdown("---")
     PTH=st.text_input("Phụ Thuộc Hàm:",help="2 vế cách nhau bằng dấu cách. mỗi PTH cách nhau bằng dấu phẩy (,)")
     st.markdown("---")
     PTH=Xuly.chuanhoa(PTH.upper())
+    run = st.button("Run")
+    init=True
  
 
 
-else:
+elif page=="Nhập Bằng File":
     input=st.text_input("Xin hãy nhập tên file",help="lưu ý:nhớ để file vào folder UMA_ENVI và đúng định dạng")
     st.markdown("---")
     st.write("Dòng Đầu file là Quan Hệ")
@@ -30,14 +33,31 @@ else:
         st.error("File Không hợp lệ hoặc chưa được nhập")
     else: 
         name=data[0]
-        PTH=data[1]   
-run = st.button("Run")
+        PTH=data[1]
+    run = st.button("Run")
+    init=True
+else:
+    number=st.number_input("Số lượng quan hệ muốn tạo",1)
+    init=st.button("Tạo PTH và quan hệ")
+    if init:
+        st.markdown("---")
+        PTH=[]
+        st.write("Quan Hệ:")
+        name=ran.RandQuanHe()
+        st.write(name)
+        st.markdown("---")
+        st.write("Phụ thuộc hàm:")
+        for i in range(0,number):
+            PTH.append(ran.RandPTH(name))
+        view=str(set(PTH)).strip("{").strip("}")
+        st.write(view)
+    run=True
 
-if run:
+if run or init:
     st.markdown("---")
     try:
         value=Xuly.findkey(name,PTH)
-        st.write(f"Khóa của Quan hệ này là: {value}")
+        st.success(f"Khóa của Quan hệ này là: {value}")
     except Exception:
         st.write("Xin hãy nhập chính xác Quan hệ và các PTH")
 
